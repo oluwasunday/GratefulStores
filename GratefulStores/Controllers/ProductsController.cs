e951f889cae241076107c5fb5e604f3ba0124616
Oladejo Sunday <sundayoladejo13@gmail.com>
2022-02-24 20:06:33 +0100

    initial commit

---
+++ GratefulStores/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GratefulStores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    private readonly StoreContext _context;
+
+    public ProductsController(StoreContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+            return Ok(products);
+        }
+    }
+}
